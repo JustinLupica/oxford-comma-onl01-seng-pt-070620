e87500c65ddcfdcb7456c848c432d5fa169786a4
Justin Lupica <justinlupica@gmail.com>
2020-07-13 15:50:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ if array.length ==  1
   array.join(", ")
 elsif array.length == 2
   array.join(" and ")
-else array[-1] = (", and ")
+else array[-1] = "and" + array[-1]
+  array.join(', ')
 end
 end
